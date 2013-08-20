11b85639126c795b09a9a66e6a80d136f098c64f
MichaelBatton <battonm@gmx.de>
2013-08-20 16:13:45 +0200

    create new directory wit original files of EGNOS and DemoApp

---
+++ SDK Source Code_ANDROID_v3.0.0/EGNOS_SDK_Core/obj/local/armeabi/objs-debug/EGNOSSWReceiver/Satellite.o.d
@@ -0,0 +1,74 @@
+/cygdrive/e/dev_EGNOSSDK_v2/Android/Ankur_dev/EGNOSDemoApp_Android/obj/local/armeabi/objs-debug/EGNOSSWReceiver/Satellite.o: \
+ /cygdrive/e/dev_EGNOSSDK_v2/Android/Ankur_dev/EGNOSDemoApp_Android/jni/Satellite.c \
+ /cygdrive/e/dev_EGNOSSDK_v2/Android/Ankur_dev/EGNOSDemoApp_Android/jni/Satellite.h \
+ /cygdrive/e/android-ndk-r6/platforms/android-9/arch-arm/usr/include/stdio.h \
+ /cygdrive/e/android-ndk-r6/platforms/android-9/arch-arm/usr/include/sys/cdefs.h \
+ /cygdrive/e/android-ndk-r6/platforms/android-9/arch-arm/usr/include/sys/cdefs_elf.h \
+ /cygdrive/e/android-ndk-r6/platforms/android-9/arch-arm/usr/include/sys/_types.h \
+ /cygdrive/e/android-ndk-r6/platforms/android-9/arch-arm/usr/include/machine/_types.h \
+ /cygdrive/e/android-ndk-r6/platforms/android-9/arch-arm/usr/include/sys/types.h \
+ /cygdrive/e/android-ndk-r6/platforms/android-9/arch-arm/usr/include/stdint.h \
+ /cygdrive/e/android-ndk-r6/platforms/android-9/arch-arm/usr/include/linux/posix_types.h \
+ /cygdrive/e/android-ndk-r6/platforms/android-9/arch-arm/usr/include/linux/stddef.h \
+ /cygdrive/e/android-ndk-r6/platforms/android-9/arch-arm/usr/include/linux/compiler.h \
+ /cygdrive/e/android-ndk-r6/platforms/android-9/arch-arm/usr/include/asm/posix_types.h \
+ /cygdrive/e/android-ndk-r6/platforms/android-9/arch-arm/usr/include/asm/types.h \
+ /cygdrive/e/android-ndk-r6/platforms/android-9/arch-arm/usr/include/linux/types.h \
+ /cygdrive/e/android-ndk-r6/platforms/android-9/arch-arm/usr/include/machine/kernel.h \
+ /cygdrive/e/android-ndk-r6/platforms/android-9/arch-arm/usr/include/sys/sysmacros.h \
+ /cygdrive/e/android-ndk-r6/platforms/android-9/arch-arm/usr/include/math.h \
+ /cygdrive/e/android-ndk-r6/platforms/android-9/arch-arm/usr/include/limits.h \
+ /cygdrive/e/android-ndk-r6/platforms/android-9/arch-arm/usr/include/sys/limits.h \
+ /cygdrive/e/android-ndk-r6/platforms/android-9/arch-arm/usr/include/linux/limits.h \
+ /cygdrive/e/android-ndk-r6/platforms/android-9/arch-arm/usr/include/machine/internal_types.h \
+ /cygdrive/e/android-ndk-r6/platforms/android-9/arch-arm/usr/include/machine/limits.h \
+ /cygdrive/e/android-ndk-r6/platforms/android-9/arch-arm/usr/include/sys/syslimits.h \
+ /cygdrive/e/dev_EGNOSSDK_v2/Android/Ankur_dev/EGNOSDemoApp_Android/jni/Constants.h
+
+/cygdrive/e/dev_EGNOSSDK_v2/Android/Ankur_dev/EGNOSDemoApp_Android/jni/Satellite.h:
+
+/cygdrive/e/android-ndk-r6/platforms/android-9/arch-arm/usr/include/stdio.h:
+
+/cygdrive/e/android-ndk-r6/platforms/android-9/arch-arm/usr/include/sys/cdefs.h:
+
+/cygdrive/e/android-ndk-r6/platforms/android-9/arch-arm/usr/include/sys/cdefs_elf.h:
+
+/cygdrive/e/android-ndk-r6/platforms/android-9/arch-arm/usr/include/sys/_types.h:
+
+/cygdrive/e/android-ndk-r6/platforms/android-9/arch-arm/usr/include/machine/_types.h:
+
+/cygdrive/e/android-ndk-r6/platforms/android-9/arch-arm/usr/include/sys/types.h:
+
+/cygdrive/e/android-ndk-r6/platforms/android-9/arch-arm/usr/include/stdint.h:
+
+/cygdrive/e/android-ndk-r6/platforms/android-9/arch-arm/usr/include/linux/posix_types.h:
+
+/cygdrive/e/android-ndk-r6/platforms/android-9/arch-arm/usr/include/linux/stddef.h:
+
+/cygdrive/e/android-ndk-r6/platforms/android-9/arch-arm/usr/include/linux/compiler.h:
+
+/cygdrive/e/android-ndk-r6/platforms/android-9/arch-arm/usr/include/asm/posix_types.h:
+
+/cygdrive/e/android-ndk-r6/platforms/android-9/arch-arm/usr/include/asm/types.h:
+
+/cygdrive/e/android-ndk-r6/platforms/android-9/arch-arm/usr/include/linux/types.h:
+
+/cygdrive/e/android-ndk-r6/platforms/android-9/arch-arm/usr/include/machine/kernel.h:
+
+/cygdrive/e/android-ndk-r6/platforms/android-9/arch-arm/usr/include/sys/sysmacros.h:
+
+/cygdrive/e/android-ndk-r6/platforms/android-9/arch-arm/usr/include/math.h:
+
+/cygdrive/e/android-ndk-r6/platforms/android-9/arch-arm/usr/include/limits.h:
+
+/cygdrive/e/android-ndk-r6/platforms/android-9/arch-arm/usr/include/sys/limits.h:
+
+/cygdrive/e/android-ndk-r6/platforms/android-9/arch-arm/usr/include/linux/limits.h:
+
+/cygdrive/e/android-ndk-r6/platforms/android-9/arch-arm/usr/include/machine/internal_types.h:
+
+/cygdrive/e/android-ndk-r6/platforms/android-9/arch-arm/usr/include/machine/limits.h:
+
+/cygdrive/e/android-ndk-r6/platforms/android-9/arch-arm/usr/include/sys/syslimits.h:
+
+/cygdrive/e/dev_EGNOSSDK_v2/Android/Ankur_dev/EGNOSDemoApp_Android/jni/Constants.h:
