2a7516e1d607aafdda0e2ed45e65f94b1d458dda
MichaelBatton <battonm@gmx.de>
2013-08-20 17:31:48 +0200

    deleting of the wrong directory and all files

--- SDK Source Code_ANDROID_v3.0.0/EGNOS_SDK_DemoApp/obj/local/armeabi/objs-debug/EGNOSSWReceiver/Matrix.o.d
+++
@@ -1,8 +0,0 @@
-/cygdrive/e/dev_EGNOSSDK_v2/Android/Ankur_dev/EGNOSDemoApp_Android/obj/local/armeabi/objs-debug/EGNOSSWReceiver/Matrix.o: \
- /cygdrive/e/dev_EGNOSSDK_v2/Android/Ankur_dev/EGNOSDemoApp_Android/jni/Matrix.c \
- /cygdrive/e/dev_EGNOSSDK_v2/Android/Ankur_dev/EGNOSDemoApp_Android/jni/Matrix.h \
- /cygdrive/e/android-ndk-r6/platforms/android-9/arch-arm/usr/include/android/log.h
-
-/cygdrive/e/dev_EGNOSSDK_v2/Android/Ankur_dev/EGNOSDemoApp_Android/jni/Matrix.h:
-
-/cygdrive/e/android-ndk-r6/platforms/android-9/arch-arm/usr/include/android/log.h:
